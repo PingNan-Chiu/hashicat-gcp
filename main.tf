b6828ca529fcd8452d22dc8e8c0c2b546be36928
PingNan-Chiu <tony79316@hotmail.com>
2021-06-15 12:49:41 +0000

    Added vpc module

--- main.tf
+++ main.tf
@@ -120,3 +120,22 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
+
+    project_id   = var.project
+    network_name = "gaurav-network"
+
+    subnets = [
+        {
+            subnet_name           = "gaurav-subnet"
+            subnet_ip             = "10.100.10.0/24"
+            subnet_region         = var.region
+        }
+
+    ]
+
+   
+}
